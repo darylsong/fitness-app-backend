129699c7b66ed5f2f108cd3f0922340dea379f71
Daryl Song <songlwdaryl@gmail.com>
2022-08-02 17:46:38 +0800

    Fix frontend

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require("cors");
 const middleware = require("./utils/middleware");
 
-const usersRouter = require("./controllers/users");
+//const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
 const registerRouter = require("./controllers/register")
 const exercisesRouter = require("./controllers/exercises");
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 
-app.use("/api/users", usersRouter);
+//app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/register", registerRouter);
 app.use("/api/users", middleware.userExtractor, exercisesRouter);
